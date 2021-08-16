c1d2c4779706b25b97b02b74a609e42a913f7b33
ricardohirata <ricardohirata@ymail.com>
2021-08-15 21:55:44 -0300

    Inclusao na classe Evidencias os metodos: - iniciaLogCSV para criar o arquivo de log com a data, hora e minuto e escrever o cabecalho. - registraCSV para escrever uma linha de log.

--- src/test/java/utils/Evidencias.java
+++ src/test/java/utils/Evidencias.java
@@ -1,17 +1,51 @@
 package utils;
 
+import com.opencsv.CSVWriter;
 import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.Writer;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 public class Evidencias {
+    static String dataHoraSimples = new SimpleDateFormat("yyyy-MM-dd HH-mm").format(Calendar.getInstance().getTime());
 
-    public void print(WebDriver driver, String datahora,  String casoDeTeste, String nomePrint) throws IOException {
+    public void print(WebDriver driver, String casoDeTeste, String nomePrint) throws IOException {
         File foto = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
-        FileUtils.copyFile(foto, new File("target/prints/" + datahora + "/" + casoDeTeste+ "/" + nomePrint + ".png"));
+        FileUtils.copyFile(foto, new File("target/prints/" + dataHoraSimples + "/" + casoDeTeste+ "/" + nomePrint + ".png"));
+    }
+
+    //Cria o arquivo de log no inicio da execucao
+    public void iniciaLogCSV() throws IOException {
+        String[] cabecalho = {"data e hora", "caso de teste", "mensagem"};
+        Writer escritor = Files.newBufferedWriter(Paths.get("target/logs/log - " + dataHoraSimples + ".csv"));
+        CSVWriter escritorCSV = new CSVWriter(escritor);
+
+        escritorCSV.writeNext(cabecalho);
+
+        escritorCSV.flush();
+        escritor.close();
+    }
+
+    //Gravar cada linha no log
+    public void registrarCSV(String casoDeTeste, String mensagem) throws IOException {
+        String dataHora = new SimpleDateFormat("[yyyy/MM/dd HH:mm:ss.SSS]").format(Calendar.getInstance().getTime());
+
+        String[] linha = {dataHora, casoDeTeste, mensagem};
+        FileWriter escritor = new FileWriter("target/logs/log - " + dataHoraSimples + ".csv", true);
+        CSVWriter escritorCSV = new CSVWriter(escritor);
+
+        escritorCSV.writeNext(linha);
+
+        escritorCSV.flush();
+        escritor.close();
     }
 }
--- src/test/java/webTests/seleniumSimples.java
+++ src/test/java/webTests/seleniumSimples.java
@@ -4,20 +4,14 @@ package webTests;
 //2 - Biblioteca
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
 
 import java.io.IOException;
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.Locale;
 import java.util.concurrent.TimeUnit;
 
 import static org.testng.Assert.assertEquals;
-import static org.testng.Assert.assertTrue;
+
 import utils.Evidencias;
 
 //3 - Classes
@@ -25,22 +19,24 @@ public class seleniumSimples {
     //3.1 - Atributos
     WebDriver driver;           // Declarar o objeto selenium WebDriver
     Evidencias evidencias;
-    static String datahora = new SimpleDateFormat("yyyy-MM-dd HH-mm").format(Calendar.getInstance().getTime());
 
     //3.2 - Metodos e funcoes
+    @BeforeClass // Executa 1 vez apenas, no inicio da execução do script
+    public void antesDeTudo(){
+        evidencias = new Evidencias();
+        System.setProperty("webdriver.chrome.driver", "drivers/chrome/92/chromedriver.exe");  // Aponta para onde está o driver do chrome.
+    }
+
     @BeforeMethod // @Before ou After Method é executado por metodo executado. ...Test 1 vez por teste geral. ...Class por classe.
     public void iniciar(){
         //A - Incio
-        System.setProperty("webdriver.chrome.driver", "drivers/chrome/92/chromedriver.exe");  // Aponta para onde está o driver do chrome.
         driver = new ChromeDriver(); // Instancia o objeto driver como um controlador do Chrome
-        evidencias = new Evidencias();
         driver.manage().timeouts().implicitlyWait(60000, TimeUnit.MICROSECONDS); //Espera ate 1 minuto, verificando a cada milisegundo. Se o TimeUnit for SECOUND verficará a cada segundo, assim tambem com minuto.
     }
 
     @AfterMethod
     public void finalizar(){
         //C - Encerramento
-        System.out.println("FECHANDO.....");
         driver.quit(); //encerra o objeto do selenium
     }
 
@@ -49,23 +45,35 @@ public class seleniumSimples {
     public void consutalCursoCTFL() throws IOException {
         String casoDeTeste = "Consultar Curso CTFL";
 
+        evidencias.registrarCSV(casoDeTeste, "Iniciou o Teste.");
         driver.get("https://www.iterasys.com.br");
-        evidencias.print(driver, datahora, casoDeTeste,"Parte 1 - Abriu o site");
+        evidencias.registrarCSV(casoDeTeste, "Abriu o site.");
+        evidencias.print(driver, casoDeTeste,"Parte 1 - Abriu o site");
 
         driver.findElement(By.id("searchtext")).click();
+        evidencias.registrarCSV(casoDeTeste, "Clicou na caixa de pesquisa.");
         driver.findElement(By.id("searchtext")).clear();
+        evidencias.registrarCSV(casoDeTeste, "Limpou a caixa de pesquisa.");
         driver.findElement(By.id("searchtext")).sendKeys("preparatório ctfl");
-        evidencias.print(driver, datahora, casoDeTeste,"Parte 2 - Digitou preparatorio ctfl");
+        evidencias.registrarCSV(casoDeTeste, "Escreveu preparatório ctfl na caixa de pesquisa.");
+        evidencias.print(driver, casoDeTeste,"Parte 2 - Digitou preparatorio ctfl");
         driver.findElement(By.id("btn_form_search")).click();
+        evidencias.registrarCSV(casoDeTeste, "Clicou no botao da lupa.");
 
         assertEquals(driver.findElement(By.cssSelector("div.col-md-6")).getText(), "Cursos › \"preparatório ctfl\"");
-        evidencias.print(driver, datahora, casoDeTeste,"Parte 3 - Exibiu a lista de cursos");
+        evidencias.registrarCSV(casoDeTeste, "Confirmou o texto indicativo da pesquisa do curso preparatório ctfl.");
+        evidencias.print(driver, casoDeTeste,"Parte 3 - Exibiu a lista de cursos");
 
         driver.findElement(By.cssSelector("span.comprar")).click();
+        evidencias.registrarCSV(casoDeTeste, "Clicou no botão matricule-se.");
 
         assertEquals(driver.findElement(By.cssSelector("span.item-title")).getText().toUpperCase(), "PREPARATÓRIO CTFL");
+        evidencias.registrarCSV(casoDeTeste, "Confirmou o nome do curso preparatório ctfl.");
         assertEquals(driver.findElement(By.cssSelector("span.new-price")).getText(), "R$ 169,00");
-        evidencias.print(driver, datahora, casoDeTeste,"Parte 4 - Exibiu o carrinho de compras");
+        evidencias.registrarCSV(casoDeTeste, "Confirmou o preço do curso preparatorio ctfl.");
+        evidencias.print(driver, casoDeTeste,"Parte 4 - Exibiu o carrinho de compras");
+
+        evidencias.registrarCSV(casoDeTeste, "Concluiu o teste.");
     }
 
     //Procurar curso Mantis
@@ -74,23 +82,35 @@ public class seleniumSimples {
         String casoDeTeste = "Consultar Curso Mantis";
 
         //B - Realizar  o teste
+        evidencias.registrarCSV(casoDeTeste, "Iniciou o Teste.");
         driver.get("https://www.iterasys.com.br"); //.get() navegação simples; .navigator() tem mais funcoes como voltar, refazer, etc. Endereco do site completo com https
-        evidencias.print(driver, datahora, casoDeTeste,"Parte 1 - Abriu o site");
+        evidencias.registrarCSV(casoDeTeste, "Abriu o site.");
+        evidencias.print(driver, casoDeTeste,"Parte 1 - Abriu o site");
 
         driver.findElement(By.id("searchtext")).click();  //clica no campo de pesquisa (id, name, css-selector, link-text, tag)
+        evidencias.registrarCSV(casoDeTeste, "Clicou na caixa de pesquisa.");
         driver.findElement(By.id("searchtext")).clear();  //limpa o campo de pesquisa
+        evidencias.registrarCSV(casoDeTeste, "Limpou a caixa de pesquisa.");
         driver.findElement(By.id("searchtext")).sendKeys("mantis"); //Escreve "mantis" no campo
-        evidencias.print(driver, datahora, casoDeTeste,"Parte 2 - Digitou Mantis");
+        evidencias.registrarCSV(casoDeTeste, "Escreveu mantis na caixa de pesquisa.");
+        evidencias.print(driver, casoDeTeste,"Parte 2 - Digitou Mantis");
 
         driver.findElement(By.id("btn_form_search")).click(); //clica na lupa
+        evidencias.registrarCSV(casoDeTeste, "Clicou no botao da lupa.");
 
         //assertTrue(driver.findElement(By.cssSelector("div.col-md-6")).getText().contains("mantis")); assert se contem "mantis"
         assertEquals(driver.findElement(By.cssSelector("div.col-md-6")).getText(), "Cursos › \"mantis\""); // o sinal de mair so ficou igual depois de mudar File>File Properties>File Encoding>Windows-1252
-        evidencias.print(driver, datahora, casoDeTeste,"Parte 3 - Exibiu a lista de cursos");
+        evidencias.registrarCSV(casoDeTeste, "Confirmou o texto indicativo da pesquisa do curso mantis.");
+        evidencias.print(driver, casoDeTeste,"Parte 3 - Exibiu a lista de cursos");
 
         driver.findElement(By.cssSelector("span.comprar")).click(); // clica no botão matricule-se
+        evidencias.registrarCSV(casoDeTeste, "Clicou no botão matricule-se.");
         assertEquals(driver.findElement(By.cssSelector("span.item-title")).getText(), "Mantis");
+        evidencias.registrarCSV(casoDeTeste, "Confirmou o nome do curso mantis.");
         assertEquals(driver.findElement(By.cssSelector("span.new-price")).getText(), "R$ 49,99");
-        evidencias.print(driver, datahora, casoDeTeste,"Parte 4 - Exibiu o carrinho de compras");
+        evidencias.registrarCSV(casoDeTeste, "Confirmou o preço do curso mantis.");
+        evidencias.print(driver, casoDeTeste,"Parte 4 - Exibiu o carrinho de compras");
+
+        evidencias.registrarCSV(casoDeTeste, "Concluiu o teste.");
     }
 }
